b7a676a537b3e5d7e7ad15d0b3d55967eb01f38f
Oliver Joshua Jacob <oliverdgr888@gmail.com>
2018-12-27 19:58:04 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,46 @@
+jQuery(document).ready(function() {
+  $('#videolink').magnificPopup({
+    type: 'inline',
+    midClick: true
+  });
+});
+
+$(function() {
+  $('#team-members').owlCarousel({
+    items: 2,
+    autoplay: true,
+    smartSpeed: 700,
+    loop: true,
+    autoplayHoverPause: true,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      480: {
+        items: 2
+      }
+    }
+  });
+});
+
+$(function() {
+  $('#progress-elements').waypoint(
+    function() {
+      $('.progress-bar').each(function() {
+        $(this).animate(
+          {
+            width: $(this).attr('aria-valuenow') + '%'
+          },
+          2000
+        );
+      });
+
+      this.destroy();
+    },
+    {
+      offset: 'bottom-in-view'
+    }
+  );
+});
