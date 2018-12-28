2cbbc0fe2cee1d7f2a640412e7ed64352ae4f749
Oliver Joshua Jacob <oliverdgr888@gmail.com>
2018-12-28 23:51:35 +0530

    added gallery section

--- main.js
+++ main.js
@@ -47,4 +47,40 @@ $(function () {
   $("#services-tabs").responsiveTabs({
     animation: 'slide'
   });
+
+  $('.gallery').magnificPopup({
+    delegate: '.popimg',
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+  });
+  $('.gallery').isotope({
+    // options
+    itemSelector: '.items'
+  });
+
+  var $gallery = $('.gallery').isotope({
+    // options
+  });
+
+  // filter items on button click
+  $('.filtering').on('click', 'span', function () {
+
+    var filterValue = $(this).attr('data-filter');
+
+    $gallery.isotope({
+      filter: filterValue
+    });
+
+  });
+
+  $('.filtering').on('click', 'span', function () {
+
+    $(this).addClass('active').siblings().removeClass('active');
+
+  });
+
+
+
 });
