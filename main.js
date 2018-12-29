f9a3d1068fcb68c1e3ce76273636128439d1b2d6
Oliver Joshua Jacob <oliverdgr888@gmail.com>
2018-12-30 02:39:48 +0530

    added blog post section

--- main.js
+++ main.js
@@ -119,4 +119,15 @@ $(function () {
       }
     }
   });
+
+  $('#news-slider').owlCarousel({
+    items: 3,
+    itemsDesktop: [1199, 3],
+    itemsDesktopSmall: [1000, 2],
+    itemsMobile: [600, 1],
+    pagination: false,
+    navigation: false,
+    autoplay: true
+
+  });
 });
