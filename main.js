927e9e5caacecb84ec35d9148ceee4fa745f3ca7
Oliver Joshua Jacob <oliverdgr888@gmail.com>
2019-01-04 11:47:42 +0530

    added nav menu and smooth scroll

--- main.js
+++ main.js
@@ -130,4 +130,49 @@ $(function () {
     autoplay: true
 
   });
+
+});
+
+jQuery(document).ready(function () {
+  $(window).scroll(function () {
+
+    if ($(window).scrollTop() > 80) {
+      $('.main_header').addClass('sticky');
+    } else {
+      $('.main_header').removeClass('sticky');
+    }
+  });
+
+
+  $('.mobile-toggle').click(function () {
+    if ($('.main_header').hasClass('open-nav')) {
+      $('.main_header').removeClass('open-nav');
+    } else {
+      $('.main_header').addClass('open-nav');
+    }
+  });
+
+
+
+
+
+  $('.main_header li a').click(function () {
+    if ($('.main_header').hasClass('open-nav')) {
+      $('.navigation').removeClass('open-nav');
+      $('.main_header').removeClass('open-nav');
+    }
+  });
+
+
+  $('nav a').click(function (event) {
+    var id = $(this).attr("href");
+    var offset = 0;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+      scrollTop: target
+    }, 600);
+    event.preventDefault();
+  });
+
+  new WOW().init();
 });
